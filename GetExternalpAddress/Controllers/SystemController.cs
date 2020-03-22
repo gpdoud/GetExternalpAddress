a62f0ee3ea825bf4c66713ac0e3eea459ed93081
Greg Doud <gpdoud@gmail.com>
2020-03-21 22:08:47 -0400

    Add project files.

---
+++ GetExternalpAddress/Controllers/SystemController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetExternalpAddress.Controllers {
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase {
+
+        [HttpGet("/")]
+        public IActionResult GetIp() {
+            return new JsonResult(new { ip = Request.HttpContext.Connection.RemoteIpAddress.ToString() });
+        }
+    }
+}
